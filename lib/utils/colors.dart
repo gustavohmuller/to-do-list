5f653a261e44c6ebd5ebb25ebc25e8149874436c
Gustavo Müller <gustavomuller@mx2.unisc.br>
2022-11-16 17:52:44 -0300

    feat: create color palette

---
+++ lib/utils/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const Color black = Color(0XFF000000);
+const Color lessThanTwoDays = Color(0XFFFF0000);
+const Color lessThanFourDays = Color(0XFFFFFF00);
+const Color sevenDaysOrMore = Color(0XFF00FF00);
+const Color taskDone = Color(0XFF2E3440);
+const Color taskExpired = Color(0XFF000000);
+const Color fab = Color(0XFF2E3440);
+const Color checkbox = Color(0XFF2E3440);
